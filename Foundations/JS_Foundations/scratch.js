70147767fd713aa13d000e3fcaab862baa7d7a2d
EricTGoto <erictomokigoto@gmail.com>
2022-01-22 19:22:32 -0800

    started learning about DOM and how to manipulate it

--- Foundations/JS_Foundations/scratch.js
+++ Foundations/JS_Foundations/scratch.js
@@ -1,3 +1,11 @@
 let test = "hi"
 console.log(test)
 console.log("hi")
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
