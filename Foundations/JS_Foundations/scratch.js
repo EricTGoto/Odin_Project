65f8f0403264aeab13c4709e3ba9286f19286c87
EricTGoto <erictomokigoto@gmail.com>
2022-01-23 21:11:10 -0800

    learned about DOM and callbacks, event listeners

--- Foundations/JS_Foundations/scratch.js
+++ Foundations/JS_Foundations/scratch.js
@@ -9,3 +9,50 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const redP = document.createElement('p');
+redP.classList.add('redP');
+redP.textContent = "Hey I'm red!";
+redP.style.color = 'red';
+container.appendChild(redP)
+
+const blueH3 = document.createElement('h3');
+blueH3.classList.add('blueH3')
+blueH3.textContent = "I'm a blue h3!"
+blueH3.style.color = 'blue';
+container.appendChild(blueH3);
+
+const div = document.createElement('div');
+div.classList.add('newStyle');
+div.style.background = 'pink';
+div.style.border = '1px solid black';
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+const pInDiv = document.createElement('p');
+pInDiv.textContent = "ME TOO!";
+div.appendChild(pInDiv);
+
+const button = document.createElement('button');
+button.textContent = "hi i'm a button";
+
+
+function alertFunction() {
+    alert("CLICKED");
+}
+
+button.addEventListener('click', function(e) {
+    e.target.style.background = 'blue';
+});
+div.appendChild(button);
+
+container.appendChild(div);
+
+const buttons = document.querySelectorAll('.pancake');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.textContent);
+    });
+});
